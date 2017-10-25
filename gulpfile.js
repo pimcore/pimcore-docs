40c0f40d9c758d7946535c3542f20de078495382
Mathias Geat <mathias.geat@elements.at>
2017-10-25 15:15:16 +0200

    Set up gulp for pimcore theme
    Copied daux' gulpfile and altered resources path

---
+++ gulpfile.js
@@ -0,0 +1,84 @@
+var cssnano = require('cssnano'),
+    gulp = require('gulp'),
+    less = require('gulp-less'),
+    rename = require('gulp-rename'),
+    plumber = require('gulp-plumber'),
+    postcss = require('gulp-postcss'),
+    stylelint = require('gulp-stylelint');
+
+var resources = {
+    pimcore: {
+        source: "themes/pimcore/less/pimcore.less",
+        dest: "themes/pimcore/css/"
+    }
+};
+
+var stylelintRules = {
+    "indentation": 4,
+    "selector-list-comma-newline-after": "always-multi-line",
+    "selector-max-id": 0,
+
+    // Autoprefixer
+    "at-rule-no-vendor-prefix": true,
+    "media-feature-name-no-vendor-prefix": true,
+    "property-no-vendor-prefix": true,
+    "selector-no-vendor-prefix": true,
+    "value-no-vendor-prefix": true
+};
+
+var cssnanoOptions = {
+    safe: true,           // Disable dangerous optimisations
+    filterPlugins: false, // This does very weird stuff
+    autoprefixer: {
+        add: true,        // Add needed prefixes
+        remove: true      // Remove unnecessary prefixes
+    }
+};
+
+function createCSSTask(source, dest) {
+    return function () {
+        return gulp.src(source)
+            .pipe(plumber())
+            .pipe(less())
+            .pipe(postcss([cssnano(cssnanoOptions)]))
+            .pipe(rename({suffix: '.min'}))
+            .pipe(gulp.dest(dest));
+    }
+}
+
+gulp.task('lint-css', function () {
+    return gulp
+        .src(['themes/**/less/**/*.less', '!themes/**/vendor/**/*.less'])
+        .pipe(stylelint({
+            failAfterError: true,
+            config: {
+                extends: 'stylelint-config-standard',
+                rules: stylelintRules
+            },
+            syntax: 'less',
+            reporters: [{
+                formatter: 'string',
+                console: true
+            }],
+            debug: true
+        }));
+});
+
+var style_tasks = [];
+for (var style in resources) {
+    if (resources.hasOwnProperty(style)) {
+        gulp.task('style_' + style, createCSSTask(resources[style].source, resources[style].dest));
+        style_tasks.push('style_' + style);
+    }
+}
+
+style_tasks.push('lint-css');
+
+gulp.task("styles", style_tasks);
+
+gulp.task('watch', ['default'], function () {
+    // Watch .less files
+    gulp.watch('themes/**/less/**/*.less', ['styles']);
+});
+
+gulp.task('default', ['styles']);
