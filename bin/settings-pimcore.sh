fc8658f43a4cfa6c084211d3b4604e7b6f951269
Christian Fasching <christian.fasching@pimcore.com>
2021-04-10 14:51:31 +0200

    added pimcore 6.9

--- bin/settings-pimcore.sh
+++ bin/settings-pimcore.sh
@@ -2,10 +2,10 @@
 
 # all paths relative to pimcore-docs/bin
 
-tags=(       "master"  "6.8"           "v6.7.3"  "v6.6.11" "v6.5.3" "v6.4.2" "v6.3.6" "v6.2.2" "v6.1.2" "v6.0.5" "v5.8.9" "4.6.5" )
-versions=(   "master"  "current"       "6.7"     "6.6"     "6.5"    "6.4"    "6.3"    "6.2"    "6.1"    "6.0"    "5.8"    "4.6"   )
-labels=(     "master"  "6.8 (current)" "6.7"     "6.6"     "6.5"    "6.4"    "6.3"    "6.2"    "6.1"    "6.0"    "5.8"    "4.6"   )
-maintained=( "true"    "true"          "false"   "false"   "false"  "false"  "false"  "false"  "false"  "false"  "false"  "false" )
+tags=(       "master"              "6.9"           "v6.8.12" "v6.7.3"  "v6.6.11" "v6.5.3" "v6.4.2" "v6.3.6" "v6.2.2" "v6.1.2" "v6.0.5" "v5.8.9" "4.6.5" )
+versions=(   "master"              "current"       "6.8"     "6.7"     "6.6"     "6.5"    "6.4"    "6.3"    "6.2"    "6.1"    "6.0"    "5.8"    "4.6"   )
+labels=(     "master (Pimcore X)"  "6.9 (current)" "6.8"     "6.7"     "6.6"     "6.5"    "6.4"    "6.3"    "6.2"    "6.1"    "6.0"    "5.8"    "4.6"   )
+maintained=( "true"                "true"          "false"   "false"   "false"   "false"  "false"  "false"  "false"  "false"  "false"  "false"  "false" )
 
 repository="pimcore"
 outputPath="/docs/pimcore"
