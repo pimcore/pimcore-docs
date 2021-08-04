3f016195f3518c65f1afa29cee28a73e9c9c5c68
Bernhard Rusch <bernhard.rusch@elements.at>
2021-08-04 17:44:31 +0200

    Actions

--- bin/do-generate.sh
+++ bin/do-generate.sh
@@ -19,59 +19,54 @@ git pull
 # get length of an array
 arraylength=${#tags[@]}
 
-# use for loop to read all values and indexes
-for (( i=0; i<${arraylength}; i++ ));
+# we need to run the prepare command twice, so that we have an up-to-date version map file
+for (( i=0; i<2; i++ ));
 do
-  printf "\n\n======================================================\n"
-  echo "Preparing version: " ${tags[$i]} " : " ${versions[$i]}
-  printf "=======================================================\n"
+  # use for loop to read all values and indexes
+  for (( i=0; i<${arraylength}; i++ ));
+  do
+    printf "\n\n======================================================\n"
+    echo "Preparing version: " ${tags[$i]} " : " ${versions[$i]}
+    printf "=======================================================\n"
 
-  cd $cwd
-  cd ../../repos/${repository}
+    cd $cwd
+    cd ../../repos/${repository}
 
-  git checkout ${tags[$i]}
-  git pull
-  git reset --hard
-  git clean -df
+    git checkout ${tags[$i]}
+    git pull
+    git reset --hard
+    git clean -df
 
-  configFileFull=${configFilePath}"/"${configFileBaseName}"-"${versions[$i]}".json"
-  configFileMajor=${configFilePath}"/"${configFileBaseName}"-"${versions[$i]:0:1}".json"
+    configFileFull=${configFilePath}"/"${configFileBaseName}"-"${versions[$i]}".json"
+    configFileMajor=${configFilePath}"/"${configFileBaseName}"-"${versions[$i]:0:1}".json"
 
-  cd $cwd
+    cd $cwd
 
-  if [  -e ${configFileFull} ]; then
-    workingConfigFile=${configFileFull}
-  elif [ -e ${configFileMajor}  ]; then
-    workingConfigFile=${configFileMajor}
-  else
-     workingConfigFile=${configFilePath}"/"${configFileDefault}
-  fi
+    if [  -e ${configFileFull} ]; then
+      workingConfigFile=${configFileFull}
+    elif [ -e ${configFileMajor}  ]; then
+      workingConfigFile=${configFileMajor}
+    else
+       workingConfigFile=${configFilePath}"/"${configFileDefault}
+    fi
 
-   docFolder="../../repos/"${repository}"/doc"
-   if [ ! -e ${docFolder} ]; then
-      docFolder="../../repos/"${repository}"/docs"
-   fi
+     docFolder="../../repos/"${repository}"/doc"
+     if [ ! -e ${docFolder} ]; then
+        docFolder="../../repos/"${repository}"/docs"
+     fi
 
-   buildFolder="../../build/"${repository}"/"${versions[$i]}
-   mkdir -p ${buildFolder}
+     buildFolder="../../build/"${repository}"/"${versions[$i]}
+     mkdir -p ${buildFolder}
 
-  ./console prepare --clear-build-dir ${additionalPrepareConfigs} --config-file=${workingConfigFile} \
-     --repository-version=${versions[$i]} --version-map-file=./repos/versionmaps/${repository}.json \
-     --repository-version-label="${labels[$i]}" --repository-version-maintained=${maintained[$i]} \
-     --version-switch-path-prefix=${outputPath} ${docFolder} ${buildFolder}
+    ./console prepare --clear-build-dir ${additionalPrepareConfigs} --config-file=${workingConfigFile} \
+       --repository-version=${versions[$i]} --version-map-file=./repos/versionmaps/${repository}.json \
+       --repository-version-label="${labels[$i]}" --repository-version-maintained=${maintained[$i]} \
+       --version-switch-path-prefix=${outputPath} ${docFolder} ${buildFolder}
 
-  # run prepare twice to have an up to date version map
-  ./console prepare --clear-build-dir ${additionalPrepareConfigs} --config-file=${workingConfigFile} \
-     --repository-version=${versions[$i]} --version-map-file=./repos/versionmaps/${repository}.json \
-     --repository-version-label="${labels[$i]}" --repository-version-maintained=${maintained[$i]} \
-     --version-switch-path-prefix=${outputPath} ${docFolder} ${buildFolder}
-  ./console prepare --clear-build-dir ${additionalPrepareConfigs} --config-file=${workingConfigFile} \
-     --repository-version=${versions[$i]} --version-map-file=./repos/versionmaps/${repository}.json \
-     --repository-version-label="${labels[$i]}" --repository-version-maintained=${maintained[$i]} \
-     --version-switch-path-prefix=${outputPath} ${docFolder} ${buildFolder}
-
-  cat ./repos/versionmaps/${repository}.json
+    cat ./repos/versionmaps/${repository}.json
+  done
 done
+
 
 
 # cleanup output directory
