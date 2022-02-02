25dc2ebccd05765f845213211c574994f5ebed83
Christian Fasching <christian.fasching@pimcore.com>
2022-02-02 14:02:05 +0100

    update doc versions

--- bin/settings-portal-engine.sh
+++ bin/settings-portal-engine.sh
@@ -2,10 +2,10 @@
 
 # all paths relative to pimcore-docs/bin
 
-tags=(      "2.x"        "2.1"           "v2.0.5" "v1.0.4" )
-versions=(  "2.2"        "current"       "2.0"    "1.0" )
-labels=(    "2.2 (dev)"  "2.1 (current)" "2.0"    "1.0" )
-maintained=("true"       "true"          "false"  "false" )
+tags=(      "2.x"        "2.2"           "v2.1.8" "v2.0.5" "v1.0.4" )
+versions=(  "2.3"        "current"       "2.1"    "2.0"    "1.0" )
+labels=(    "2.3 (dev)"  "2.2 (current)" "2.1"    "2.0"    "1.0" )
+maintained=("true"       "true"          "false"  "false"  "false" )
 
 gitRepository="git@github.com:pimcore/portal-engine.git"
 repository="portal-engine"
--- bin/settings-statistics-explorer.sh
+++ bin/settings-statistics-explorer.sh
@@ -3,10 +3,10 @@
 # all paths relative to pimcore-docs/bin
 
 
-tags=(       "1.x"        "1.3"           "v1.2.0" )
-versions=(   "1.4"        "current"       "1.2" )
-labels=(     "1.4 (dev)"  "1.3 (current)" "1.2" )
-maintained=( "true"       "true"          "false" )
+tags=(       "1.x"        "1.4"           "v1.3.2" "v1.2.0" )
+versions=(   "1.5"        "current"       "1.3"    "1.2" )
+labels=(     "1.5 (dev)"  "1.4 (current)" "1.3"    "1.2" )
+maintained=( "true"       "true"          "false"  "false" )
 
 gitRepository="git@github.com:pimcore/statistics-explorer.git"
 repository="statistics-explorer"
