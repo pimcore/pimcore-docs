a66a1285b32e4b5532e37c4328960861724e70b7
mcop1 <89011527+mcop1@users.noreply.github.com>
2022-04-28 10:31:28 +0200

    Update settings-data-hub-simple-rest.sh

--- bin/settings-data-hub-simple-rest.sh
+++ bin/settings-data-hub-simple-rest.sh
@@ -2,9 +2,9 @@
 
 # all paths relative to pimcore-docs/bin
 
-tags=(       "1.x"        "1.5"           "v1.4.0" "v1.3.2")
-versions=(   "1.6"        "current"       "1.4"    "1.3")
-labels=(     "1.6 (dev)"  "1.5 (current)" "1.4"    "1.3")
+tags=(       "1.x"        "1.6"           "v1.4.0" "v1.3.2")
+versions=(   "1.7"        "current"       "1.4"    "1.3")
+labels=(     "1.7 (dev)"  "1.6 (current)" "1.4"    "1.3")
 maintained=( "true"       "true"          "false"  "false")
 
 gitRepository="git@github.com:pimcore/data-hub-simple-rest.git"
