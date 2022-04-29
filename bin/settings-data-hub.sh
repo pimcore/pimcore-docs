c0d2cd953297494e1dbb56e6970028b1433fc341
Christian Fasching <fashxp@users.noreply.github.com>
2022-04-29 09:19:27 +0200

    updated docs versions

--- bin/settings-data-hub.sh
+++ bin/settings-data-hub.sh
@@ -2,10 +2,10 @@
 
 # all paths relative to pimcore-docs/bin
 
-tags=(       "1.x"        "1.2"           "v1.1.7" "v1.0.11" )
-versions=(   "1.3"        "current"       "1.1"    "1.0" )
-labels=(     "1.3 (dev)"  "1.2 (current)" "1.1"    "1.0" )
-maintained=( "true"       "true"          "false"  "false" )
+tags=(       "1.x"        "1.3"           "v1.2.6" "v1.1.7" "v1.0.11" )
+versions=(   "1.4"        "current"       "1.2"    "1.1"    "1.0" )
+labels=(     "1.4 (dev)"  "1.3 (current)" "1.2"    "1.1"    "1.0" )
+maintained=( "true"       "true"          "false"  "false"  "false" )
 
 gitRepository="https://github.com/pimcore/data-hub.git"
 repository="data-hub"
