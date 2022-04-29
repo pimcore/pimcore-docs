4539664c52123e9dd039fa91e74ef585dcec3c31
Christian Fasching <fashxp@users.noreply.github.com>
2022-04-29 09:17:53 +0200

    updated doc versions

--- bin/settings-data-hub-simple-rest.sh
+++ bin/settings-data-hub-simple-rest.sh
@@ -2,10 +2,10 @@
 
 # all paths relative to pimcore-docs/bin
 
-tags=(       "1.x"        "1.6"           "v1.4.0" "v1.3.2")
-versions=(   "1.7"        "current"       "1.4"    "1.3")
-labels=(     "1.7 (dev)"  "1.6 (current)" "1.4"    "1.3")
-maintained=( "true"       "true"          "false"  "false")
+tags=(       "1.x"        "1.6"           "v1.5.4" "v1.4.0" "v1.3.2")
+versions=(   "1.7"        "current"       "1.5"    "1.4"    "1.3")
+labels=(     "1.7 (dev)"  "1.6 (current)" "1.5"    "1.4"    "1.3")
+maintained=( "true"       "true"          "false"  "false"  "false")
 
 gitRepository="git@github.com:pimcore/data-hub-simple-rest.git"
 repository="data-hub-simple-rest"
