6d0386bffdc7d3a931fb265e7d5a754886a246f9
Christian Fasching <fashxp@users.noreply.github.com>
2022-04-29 09:16:15 +0200

    updated docs versions

--- bin/settings-cmf.sh
+++ bin/settings-cmf.sh
@@ -2,10 +2,10 @@
 
 # all paths relative to pimcore-docs/bin
 
-tags=(       "3.x"        "3.1"           "3.0"   "2.6")
-versions=(   "3.2"        "current"       "3.0"   "2.6")
-labels=(     "3.2 (dev)"  "3.1 (current)" "3.0"   "2.6")
-maintained=( "true"       "true"          "false" "false")
+tags=(       "3.x"        "3.2"           "v3.1.1" "v3.0.3"  "v2.6.2")
+versions=(   "3.3"        "current"       "3.1"    "3.0"     "2.6")
+labels=(     "3.3 (dev)"  "3.2 (current)" "3.1"    "3.0"     "2.6")
+maintained=( "true"       "true"          "false"  "false"   "false")
 
 gitRepository="https://github.com/pimcore/customer-data-framework.git"
 repository="customer-data-framework"
