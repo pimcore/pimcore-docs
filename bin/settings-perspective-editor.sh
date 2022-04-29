8074de54924ced90f6543c0071f7f9688bc9fa24
Christian Fasching <fashxp@users.noreply.github.com>
2022-04-29 09:21:53 +0200

    updated docs versions

--- bin/settings-perspective-editor.sh
+++ bin/settings-perspective-editor.sh
@@ -2,10 +2,10 @@
 
 # all paths relative to pimcore-docs/bin
 
-tags=(      "1.x"          "1.2"          )
-versions=(  "1.3"          "current"      )
-labels=(    "1.3 (dev)"    "1.2 (current)")
-maintained=("true"         "true"         )
+tags=(      "1.x"          "1.3"            "v1.2.1" )
+versions=(  "1.4"          "current"        "1.2"    )
+labels=(    "1.4 (dev)"    "1.3 (current)"  "1.2"    )
+maintained=("true"         "true"           "false"  )
 
 gitRepository="https://github.com/pimcore/perspective-editor.git"
 repository="perspective-editor"
