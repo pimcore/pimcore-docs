dca709936523efa09c89b4094976e24b7dc866b6
mcop1 <89011527+mcop1@users.noreply.github.com>
2022-05-25 15:37:05 +0200

    Update settings-data-importer.sh

--- bin/settings-data-importer.sh
+++ bin/settings-data-importer.sh
@@ -2,10 +2,10 @@
 
 # all paths relative to pimcore-docs/bin
 
-tags=(      "1.x"       "1.5"            "v1.4.6" "v1.3.6" "v1.2.2" "v1.1.5" "v1.0.1" )
-versions=(  "1.6"       "current"        "1.4" "1.3" "1.2" "1.1"    "1.0")
-labels=(    "1.6 (dev)" "1.5 (current)"  "1.4" "1.3" "1.2" "1.1"    "1.0")
-maintained=("true"      "true"           "false" "false" "false" "false"  "false")
+tags=(      "1.x"       "1.6"            "1.5" "v1.4.6" "v1.3.6" "v1.2.2" "v1.1.5" "v1.0.1" )
+versions=(  "1.7"       "current"        "1.5" "1.4" "1.3" "1.2" "1.1"    "1.0")
+labels=(    "1.7 (dev)" "1.7 (current)"  "1.5" "1.4" "1.3" "1.2" "1.1"    "1.0")
+maintained=("true"      "true"           "false" "false" "false" "false" "false"  "false")
 
 gitRepository="https://github.com/pimcore/data-importer.git"
 repository="data-importer"
