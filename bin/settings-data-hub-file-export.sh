738930a3de633a03a6cff978a39e506e4bcf6a60
robertSt7 <104770750+robertSt7@users.noreply.github.com>
2022-07-06 16:14:33 +0200

    Update settings-data-hub-file-export.sh

--- bin/settings-data-hub-file-export.sh
+++ bin/settings-data-hub-file-export.sh
@@ -2,10 +2,10 @@
 
 # all paths relative to pimcore-docs/bin
 
-tags=(       "1.x"       "1.4"           "v1.3.1" "v1.2.2" )
-versions=(   "1.5"       "current"       "1.3"    "1.2"    )
-labels=(     "1.5 (dev)" "1.4 (current)" "1.3"    "1.2"    )
-maintained=( "true"      "true"          "false"  "false"  )
+tags=(       "1.x"       "1.5"           "v1.4.3"  "v1.3.1" "v1.2.2" )
+versions=(   "1.6"       "current"       "1.4"     "1.3"    "1.2"    )
+labels=(     "1.6 (dev)" "1.5 (current)" "1.4"     "1.3"    "1.2"    )
+maintained=( "true"      "true"          "false"   "false"  "false"  )
 
 gitRepository="git@github.com:pimcore/data-hub-file-export.git"
 repository="data-hub-file-export"
