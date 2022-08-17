1dfdc0aa07aecd1d339e2a8f2ee062db326912b9
mcop1 <89011527+mcop1@users.noreply.github.com>
2022-08-17 14:43:39 +0200

    Update settings-data-hub.sh

--- bin/settings-data-hub.sh
+++ bin/settings-data-hub.sh
@@ -2,10 +2,10 @@
 
 # all paths relative to pimcore-docs/bin
 
-tags=(       "1.x"        "1.4"           "v1.3.2" "v1.2.6" "v1.1.7" "v1.0.11" )
-versions=(   "1.5"        "current"       "1.3" "1.2"    "1.1"    "1.0" )
-labels=(     "1.5 (dev)"  "1.4 (current)" "1.3" "1.2"    "1.1"    "1.0" )
-maintained=( "true"       "true"          "false" "false"  "false"  "false" )
+tags=(       "1.x"        "1.5"           "v1.4.5" "v1.3.2" "v1.2.6" "v1.1.7" "v1.0.11" )
+versions=(   "1.6"        "current"       "1.4" "1.3" "1.2"    "1.1"    "1.0" )
+labels=(     "1.6 (dev)"  "1.5 (current)" "1.4" "1.3" "1.2"    "1.1"    "1.0" )
+maintained=( "true"       "true"          "false" "false" "false"  "false"  "false" )
 
 gitRepository="https://github.com/pimcore/data-hub.git"
 repository="data-hub"
