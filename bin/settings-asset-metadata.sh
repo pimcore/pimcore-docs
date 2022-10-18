37710a503e0453b31a2c83a3eaa0a86902fa9459
Bernhard Rusch <bernhard.rusch@elements.at>
2022-10-18 14:51:10 +0200

    Fixed pimcore/asset-metadata-class-definitions

--- bin/settings-asset-metadata.sh
+++ bin/settings-asset-metadata.sh
@@ -4,10 +4,10 @@ set -ex
 
 # all paths relative to pimcore-docs/bin
 
-tags=(      "1.x"        "1.1"            "v1.0.2")
-versions=(  "1.2"        "current"        "1.0")
-labels=(    "1.2 (dev)"  "1.1 (current)"  "1.0")
-maintained=("true"       "true"           "false")
+tags=(      "2.x"        "1.2"            "v1.1.2"        "v1.0.2")
+versions=(  "2.0"        "current"        "1.1"        "1.0")
+labels=(    "2.0 (dev)"  "1.2 (current)"  "1.1"        "1.0")
+maintained=("true"       "true"           "false"      "false")
 
 gitRepository="git@github.com:pimcore/asset-metadata-class-definitions.git"
 repository="asset-metadata-class-definitions"
