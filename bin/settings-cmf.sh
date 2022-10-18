783d57eeabcd9a17522fdeb5ad087a3957fa98c7
Divesh Pahuja <divesh.pahuja@pimcore.com>
2022-10-18 15:33:09 +0200

    Update docs version - pimcore/customer-data-framework

--- bin/settings-cmf.sh
+++ bin/settings-cmf.sh
@@ -4,10 +4,10 @@ set -ex
 
 # all paths relative to pimcore-docs/bin
 
-tags=(       "4.x"        "3.3"           "v3.2.14" "v3.1.1" "v3.0.3"  "v2.6.2")
-versions=(   "4.0"        "current"       "3.2"     "3.1"    "3.0"     "2.6")
-labels=(     "4.0 (dev)"  "3.3 (current)" "3.2"     "3.1"    "3.0"     "2.6")
-maintained=( "true"       "true"          "false"   "false"  "false"   "false")
+tags=(       "4.x"        "3.3"           "v3.2.14" "v3.1.1" "v3.0.3")
+versions=(   "4.0"        "current"       "3.2"     "3.1"    "3.0")
+labels=(     "4.0 (dev)"  "3.3 (current)" "3.2"     "3.1"    "3.0")
+maintained=( "true"       "true"          "false"   "false"  "false")
 
 gitRepository="https://github.com/pimcore/customer-data-framework.git"
 repository="customer-data-framework"
