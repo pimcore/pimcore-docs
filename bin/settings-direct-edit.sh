24f69b1a8d573a844e1b0ddb3ae2490d23fd5ace
Christian Fasching <fashxp@users.noreply.github.com>
2022-10-19 09:54:21 +0200

    Update settings-direct-edit.sh

--- bin/settings-direct-edit.sh
+++ bin/settings-direct-edit.sh
@@ -5,9 +5,9 @@ set -ex
 # all paths relative to pimcore-docs/bin
 
 
-tags=(       "1.x"        "1.2"           "v1.1.0" )
-versions=(   "1.3"        "current"       "1.1" )
-labels=(     "1.3 (dev)"  "1.2 (current)" "1.1" )
+tags=(       "2.x"        "1.2"           "v1.1.0" )
+versions=(   "2.0"        "current"       "1.1" )
+labels=(     "2.0 (dev)"  "1.2 (current)" "1.1" )
 maintained=( "true"       "true"          "false" )
 
 gitRepository="git@github.com:pimcore/direct-edit.git"
