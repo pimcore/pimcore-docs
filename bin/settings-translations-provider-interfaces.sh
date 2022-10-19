2198f0aee474071666f67e4d8d52691239c21f2d
Christian Fasching <fashxp@users.noreply.github.com>
2022-10-19 10:58:14 +0200

    Update settings-translations-provider-interfaces.sh

--- bin/settings-translations-provider-interfaces.sh
+++ bin/settings-translations-provider-interfaces.sh
@@ -4,7 +4,7 @@ set -ex
 
 # all paths relative to pimcore-docs/bin
 
-tags=(       "2.x"        "2.6"           "v2.5.2"   "v2.4.2"   "v2.3.1"   "v2.2.3")
+tags=(       "2.x"        "2.6"           "v2.5.2"   "v2.4.3"   "v2.3.1"   "v2.2.3")
 versions=(   "2.7"        "current"       "2.5"      "2.4"     "2.3"      "2.2")
 labels=(     "2.7 (dev)"  "2.6 (current)" "2.5"      "2.4"     "2.3"      "2.2")
 maintained=( "true"       "true"          "false"    "false"   "false"    "false")
