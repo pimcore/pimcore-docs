02597289b62a5c0cbc10f6f82b56aae1463e5055
Christian Fasching <fashxp@users.noreply.github.com>
2022-10-19 09:56:48 +0200

    Update settings-statistics-explorer.sh

--- bin/settings-statistics-explorer.sh
+++ bin/settings-statistics-explorer.sh
@@ -5,9 +5,9 @@ set -ex
 # all paths relative to pimcore-docs/bin
 
 
-tags=(       "1.x"        "1.4"           "v1.3.2" "v1.2.0" )
-versions=(   "1.5"        "current"       "1.3"    "1.2" )
-labels=(     "1.5 (dev)"  "1.4 (current)" "1.3"    "1.2" )
+tags=(       "2.x"        "1.4"           "v1.3.2" "v1.2.0" )
+versions=(   "2.0"        "current"       "1.3"    "1.2" )
+labels=(     "2.0 (dev)"  "1.4 (current)" "1.3"    "1.2" )
 maintained=( "true"       "true"          "false"  "false" )
 
 gitRepository="git@github.com:pimcore/statistics-explorer.git"
