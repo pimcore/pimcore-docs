197528b96ec30e70019c93ad747a75d7956c0c50
Christian Fasching <fashxp@users.noreply.github.com>
2022-10-19 10:00:11 +0200

    Update settings-portal-engine.sh

--- bin/settings-portal-engine.sh
+++ bin/settings-portal-engine.sh
@@ -4,7 +4,7 @@ set -ex
 
 # all paths relative to pimcore-docs/bin
 
-tags=(      "2.x"        "2.4"           "v2.3.2" "v2.2.6" "v2.1.8" "v2.0.5" "v1.0.4" )
+tags=(      "3.x"        "2.4"           "v2.3.2" "v2.2.6" "v2.1.8" "v2.0.5" "v1.0.4" )
 versions=(  "3.0"        "current"       "2.3"    "2.2"    "2.1"    "2.0"    "1.0" )
 labels=(    "3.0 (dev)"  "2.4 (current)" "2.3"    "2.2"    "2.1"    "2.0"    "1.0" )
 maintained=("true"       "true"          "false"  "false"  "false"  "false"  "false" )
