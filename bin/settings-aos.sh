598c38a47dafeadee68484753fe1bb99089df344
Christian Fasching <fashxp@users.noreply.github.com>
2022-10-21 14:44:11 +0200

    Update settings-aos.sh

--- bin/settings-aos.sh
+++ bin/settings-aos.sh
@@ -4,9 +4,9 @@ set -ex
 
 # all paths relative to pimcore-docs/bin
 
-tags=(      "4.x"        "4.3"             "v4.2.8" "v4.1.0"  "v4.0.2"  "v3.3.1" )
-versions=(  "4.4"        "current"         "4.2"    "4.1"     "4.0"     "3.3"    )
-labels=(    "4.4 (dev)"  "4.3 (current)"   "4.2"    "4.1"     "4.0"     "3.3"    )
+tags=(      "5.x"        "4.3"             "v4.2.8" "v4.1.0"  "v4.0.2"  "v3.3.1" )
+versions=(  "5.0"        "current"         "4.2"    "4.1"     "4.0"     "3.3"    )
+labels=(    "5.0 (dev)"  "4.3 (current)"   "4.2"    "4.1"     "4.0"     "3.3"    )
 maintained=("true"       "true"            "false"  "false"   "false"   "false"  )
 
 gitRepository="https://github.com/pimcore/advanced-object-search.git"
