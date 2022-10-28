c8a2eab29f35b6579474e7f1964e281734f0671e
mcop1 <89011527+mcop1@users.noreply.github.com>
2022-10-28 09:21:15 +0200

    Update settings-perspective-editor.sh

--- bin/settings-perspective-editor.sh
+++ bin/settings-perspective-editor.sh
@@ -4,9 +4,9 @@ set -ex
 
 # all paths relative to pimcore-docs/bin
 
-tags=(       "1.x"            "1.4"              "v1.3.2"        "v1.2.1" )
-versions=(   "1.5"            "current"          "1.3"           "1.2"    )
-labels=(     "1.5 (dev=)"     "1.4 (current)"    "1.3"           "1.2"    )
+tags=(       "1.x"            "1.5"              "v1.4.2"        "v1.3.2" )
+versions=(   "1.6"            "current"          "1.4"           "1.3"    )
+labels=(     "1.6 (dev=)"     "1.5 (current)"    "1.4"           "1.3"    )
 maintained=( "true"           "true"             "false"         "false"  )
 
 gitRepository="https://github.com/pimcore/perspective-editor.git"
