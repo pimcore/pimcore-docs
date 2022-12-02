64b966810064bd6eb5d328b9e98714057b380e6c
Divesh Pahuja <divesh.pahuja@pimcore.com>
2022-12-02 11:21:56 +0100

    Update settings-translations-provider-interfaces.sh

--- bin/settings-translations-provider-interfaces.sh
+++ bin/settings-translations-provider-interfaces.sh
@@ -4,10 +4,10 @@ set -ex
 
 # all paths relative to pimcore-docs/bin
 
-tags=(       "2.x"        "2.6"           "v2.5.2"   "v2.4.3"   "v2.3.1"   "v2.2.3")
-versions=(   "2.7"        "current"       "2.5"      "2.4"     "2.3"      "2.2")
-labels=(     "2.7 (dev)"  "2.6 (current)" "2.5"      "2.4"     "2.3"      "2.2")
-maintained=( "true"       "true"          "false"    "false"   "false"    "false")
+tags=(       "3.x"        "2.7"        "2.6"           "v2.5.2"   "v2.4.3"  "v2.3.1")
+versions=(   "3.0"        "2.7"        "current"       "2.5"      "2.4"     "2.3")
+labels=(     "3.0 (dev)"  "2.7 (dev)"  "2.6 (current)" "2.5"      "2.4"     "2.3")
+maintained=( "true"       "true"       "true"          "false"    "false"   "false")
 
 gitRepository="git@github.com:pimcore/translations-provider-interfaces.git"
 repository="translations-provider-interfaces"
