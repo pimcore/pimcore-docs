ba449bfb6980c9c1da2c2ba3dd61dd47265d6d78
mcop1 <89011527+mcop1@users.noreply.github.com>
2022-12-06 11:52:05 +0100

    Update settings-data-hub-productsup.sh

--- bin/settings-data-hub-productsup.sh
+++ bin/settings-data-hub-productsup.sh
@@ -4,9 +4,9 @@ set -ex
 
 # all paths relative to pimcore-docs/bin
 
-tags=(       "1.x"        "1.3"           "v1.2.0" "v1.1.1")
-versions=(   "1.4"        "current"       "1.2"    "1.1")
-labels=(     "1.4 (dev)"  "1.3 (current)" "1.2"    "1.1")
+tags=(       "2.x"        "1.3"           "v1.2.0" "v1.1.1")
+versions=(   "2.0"        "current"       "1.2"    "1.1")
+labels=(     "2.0 (dev)"  "1.3 (current)" "1.2"    "1.1")
 maintained=( "true"       "true"          "false"  "false")
 
 gitRepository="git@github.com:pimcore/data-hub-productsup.git"
