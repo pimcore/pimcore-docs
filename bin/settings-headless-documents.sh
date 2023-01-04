cfa5015c9fd67bf5dfde142a399974a2cafa4002
Divesh Pahuja <divesh.pahuja@pimcore.com>
2023-01-04 15:09:02 +0100

    Update settings-headless-documents.sh

--- bin/settings-headless-documents.sh
+++ bin/settings-headless-documents.sh
@@ -4,10 +4,10 @@ set -ex
 
 # all paths relative to pimcore-docs/bin
 
-tags=(       "2.x"       "1.3"        "1.2"            "v1.1.3")
-versions=(   "2.0"       "1.3"        "current"        "1.1")
-labels=(     "2.0 (dev)" "1.3 (dev)"  "1.2 (current)"  "1.1")
-maintained=( "true"      "true"       "true"           "false")
+tags=(       "2.x"       "1.3"            "v1.2.1"      "v1.1.3")
+versions=(   "2.0"       "current"        "1.2"         "1.1")
+labels=(     "2.0 (dev)" "1.3 (current)"  "1.2"         "1.1")
+maintained=( "true"      "true"           "false"       "false")
 
 gitRepository="git@github.com:pimcore/headless-documents.git"
 repository="headless-documents"
