b17b89aabecb17ba091c03dd391609e9ec6a4d96
Christian Fasching <fashxp@users.noreply.github.com>
2023-02-17 10:09:22 +0100

    Update settings-data-hub-file-export.sh

--- bin/settings-data-hub-file-export.sh
+++ bin/settings-data-hub-file-export.sh
@@ -4,10 +4,10 @@ set -ex
 
 # all paths relative to pimcore-docs/bin
 
-tags=(       "2.x"       "1.5"           "v1.4.3"  "v1.3.1" "v1.2.2" )
-versions=(   "2.0"       "current"       "1.4"     "1.3"    "1.2"    )
-labels=(     "2.0 (dev)" "1.5 (current)" "1.4"     "1.3"    "1.2"    )
-maintained=( "true"      "true"          "false"   "false"  "false"  )
+tags=(       "2.x"       "1.6"           "v1.5.2"  "v1.4.3"  "v1.3.1" "v1.2.2" )
+versions=(   "2.0"       "current"       "1.5"     "1.4"     "1.3"    "1.2"    )
+labels=(     "2.0 (dev)" "1.6 (current)" "1.5"     "1.4"     "1.3"    "1.2"    )
+maintained=( "true"      "true"          "false"   "false"   "false"  "false"  )
 
 gitRepository="git@github.com:pimcore/data-hub-file-export.git"
 repository="data-hub-file-export"
