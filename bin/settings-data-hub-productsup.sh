17a472e68e200976b32f7a7ea27e7e13ff3d5534
Christian Fasching <christian.fasching@pimcore.com>
2023-05-16 21:43:57 +0200

    pimcore 11 releases

--- bin/settings-data-hub-productsup.sh
+++ bin/settings-data-hub-productsup.sh
@@ -4,7 +4,7 @@ set -ex
 
 # all paths relative to pimcore-docs/bin
 
-tags=(       "v1.3.3" "v1.2.0" "v1.1.1")
+tags=(       "v1.3.4" "v1.2.0" "v1.1.1")
 versions=(   "1.3"    "1.2"    "1.1")
 labels=(     "1.3"    "1.2"    "1.1")
 maintained=( "false"  "false"  "false")
